05a1dcbb2b09720e6986317a9d49b408ab09355d
Liz Thompson <thompsonel2011@gmail.com>
2021-04-24 12:53:51 -0400

    JS

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+// DISPLAY CURRENT DATE + TIME
+var datetime = null,
+        date = null;
+
+var update = function () {
+    date = moment(new Date())
+    datetime.html(date.format('dddd, MMMM Do YYYY, h:mm:ss a'));
+};
+
+$(document).ready(function(){
+    datetime = $('#datetime')
+    update();
+    setInterval(update, 1000);
+});
