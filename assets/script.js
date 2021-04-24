7fed1ad907c5ba69cb4a0dd14288fc2c4f6a7e08
slown137 <s.lown137@gmail.com>
2021-04-24 12:52:37 -0400

    added card and table

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+var datetime = null,
+        date = null;
+
+var update = function () {
+    date = moment(new Date())
+    datetime.html(date.format('dddd, MMMM Do YYYY, h:mm:ss a'));
+};
+
+$(document).ready(function(){
+    datetime = $('#datetime')
+    update();
+    setInterval(update, 1000);
+});
